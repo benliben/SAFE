d082ef61771c3f73dd72926901e26fc7d849a01b
junjie <1287550045@qq.com>
2016-08-01 08:56:21 +0800

    添加了home页面的布局

--- app/src/main/java/com/android/benben/safe/ui/activity/HomeActivity.java
+++ app/src/main/java/com/android/benben/safe/ui/activity/HomeActivity.java
@@ -6,9 +6,11 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
+import android.view.View;
 
 import com.android.benben.safe.R;
 import com.android.benben.safe.ui.adapter.HomeAdapter;
+import com.android.benben.safe.utils.ToastUrl;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +26,16 @@ public class HomeActivity extends AppCompatActivity {
     private List<String> mDatas;
     private HomeAdapter mAdapter;
 
+    private String[] titleStr = {
+            "手机防盗","通信卫士","软件管理",
+            "进程管理","流量统计","手机杀毒",
+            "缓存清理","高级工具","设置中心"};
+    private int[]mipmapIds={
+            R.mipmap.home_safe,R.mipmap.home_callmsgsafe,R.mipmap.home_apps,
+            R.mipmap.home_taskmanager,R.mipmap.home_netmanager,R.mipmap.home_trojan,
+            R.mipmap.home_sysoptimize,R.mipmap.home_tools,R.mipmap.home_settings
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,15 +46,39 @@ public class HomeActivity extends AppCompatActivity {
         /*设置管理器*/
         mContent.setLayoutManager(new GridLayoutManager(this, 3));
          /*设置适配器*/
-        mContent.setAdapter(mAdapter = new HomeAdapter(this,mDatas));
+        mContent.setAdapter(mAdapter = new HomeAdapter(this,titleStr,mipmapIds));
            /*设置Item增加，移除动画*/
         mContent.setItemAnimator(new DefaultItemAnimator());
+        initClick();
+    }
+
+    private void initClick() {
+        mAdapter.setOnItemClickListener(new HomeAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                switch (position) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                }
+                ToastUrl.show(getApplicationContext(), "你点击了" + position);
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+                ToastUrl.show(getApplicationContext(), "你长按了" + position);
+
+            }
+        });
     }
 
     private void initData() {
-        mDatas = new ArrayList<>();
-        for (int i = 0; i < 9; i++) {
-            mDatas.add("" + i);
-        }
+
+//        for (int i = 0; i < titleStr.length; i++) {
+//            mDatas.add("" + i);
+//        }
     }
+
+
 }
--- app/src/main/java/com/android/benben/safe/ui/adapter/HomeAdapter.java
+++ app/src/main/java/com/android/benben/safe/ui/adapter/HomeAdapter.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.benben.safe.R;
+import com.android.benben.safe.utils.ToastUrl;
 
 import java.util.List;
 
@@ -19,6 +20,15 @@ import java.util.List;
 public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
     private Context mContext;
     private List<String> mData;
+    private String[] titleStr ;
+    private int[]mipmapIds;
+
+
+    public HomeAdapter(Context mContext, String[] titleStr, int[] mipmapIds) {
+        this.mContext = mContext;
+        this.titleStr = titleStr;
+        this.mipmapIds = mipmapIds;
+    }
 
     public HomeAdapter(Context mContext, List<String> mData) {
         this.mContext = mContext;
@@ -32,13 +42,32 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(HomeAdapter.MyViewHolder holder, int position) {
-        holder.tv.setText(mData.get(position));
+    public void onBindViewHolder(final HomeAdapter.MyViewHolder holder, final int position) {
+        holder.tv.setText(titleStr[position]);
+        holder.iv.setBackgroundResource(mipmapIds[position]);
+        /*如果设置了回调 则设置点击事件*/
+        if (mOnItemClickListener != null) {
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickListener.onItemClick(holder.itemView,pos);
+                }
+            });
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickListener.onItemLongClick(holder.itemView, pos);
+                    return false;
+                }
+            });
+        }
     }
 
     @Override
     public int getItemCount() {
-        return mData.size();
+        return titleStr.length;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
@@ -47,7 +76,23 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> 
         public MyViewHolder(View itemView) {
             super(itemView);
             iv = (ImageView) itemView.findViewById(R.id.iv_home);
+            /**
+             * 有问题 需要解决
+             */
             tv = (TextView) itemView.findViewById(R.id.tv_home);
         }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+
+        void onItemLongClick(View view, int position);
+    }
+
+    private OnItemClickListener mOnItemClickListener;
+
+
+    public void setOnItemClickListener(OnItemClickListener mOnItemClickListener) {
+        this.mOnItemClickListener = mOnItemClickListener;
     }
 }
