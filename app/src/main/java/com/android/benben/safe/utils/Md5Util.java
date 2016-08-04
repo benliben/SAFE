51673746838c75a94b9f5d385d9a342848bf76bc
BenBen <3187683623@qq.com>
2016-08-04 21:58:06 +0800

    添加了MD5加密的util

--- app/src/main/java/com/android/benben/safe/utils/Md5Util.java
+++ app/src/main/java/com/android/benben/safe/utils/Md5Util.java
@@ -1,18 +1,14 @@
-import android.util.Log;
+package com.android.benben.safe.utils;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * Created by Administrator on 2016/8/3.
+ * Created by Administrator on 2016/8/4.
+ * Md5的加密模式
  */
 public class Md5Util {
-    private static final String TAG = "lyx";
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
+    public static void main(String[] aggs) {
         /*普通模式*/
 //        String psd = "123";
         /*加盐模式*/
@@ -21,31 +17,33 @@ public class Md5Util {
     }
 
     /**
-     * 给指定字符串安装MD5算法去加密
-     *
+     * 给指定字符串安装MD%算法加密
      * @param psd 需要加密的密码
      */
     private static void encoder(String psd) {
+        StringBuffer stringBuffer = new StringBuffer();
         /*1.指定加密算法类型*/
+
         try {
             MessageDigest digest = MessageDigest.getInstance("MD5");
-            /*2.将需要加密的字符串转换成byte类型的数组，然后进行随机哈希过程*/
+            /*2.将需要加密的字符串转换成byte类型的数组，然后进行随机哈希的过程*/
             byte[] bs = digest.digest(psd.getBytes());
             /*3.通过循环遍历bs，然后让其生成32位字符串，固定写法*/
-            /*4.拼接字符串的过程*/
-            StringBuffer stringBuffer = new StringBuffer();
-            for (byte b : bs) {
+            for (byte b : bs
+                    ) {
                 int i = b & 0xff;
                 /*int类型的i需要转换成16进制字符*/
-                String hexSthing = Integer.toHexString(i);
-                if (hexSthing.length() < 2) {
-                    hexSthing = "0" + hexSthing;
+                String hexString = Integer.toHexString(i);
+                if (hexString.length() < 2) {
+                    hexString = "0" + hexString;
                 }
-                stringBuffer.append(hexSthing);
+                /*4.拼接字符串的过程*/
+                stringBuffer.append(hexString);
             }
-
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
+
     }
+
 }
